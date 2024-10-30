e61a406b9ee1678a06fae9242ef9ebd63b00a0db
user <wangyouan@xmu.edu.cn>
2024-10-30 01:24:32 +0000

    update path info

--- Constant/path_info.py
+++ Constant/path_info.py
@@ -13,18 +13,27 @@ import socket
 class PathInfo(object):
     # PROJECT_PATH = '/mnt/d/Onedrive/Temp/Projects/CapitalControl'
     COMPUTER_NAME = socket.gethostname()
-    PROJECT_PATH = r'D:\Onedrive\Temp\Projects\CapitalControl'
-    TEMP_PATH = os.path.join(PROJECT_PATH, 'temp')
-    OUTPUT_PATH = os.path.join(PROJECT_PATH, 'regression_data')
-    REGRESSION_RESULT_PATH = os.path.join(PROJECT_PATH, 'regression_results')
+    if COMPUTER_NAME == 'ubuntu-server':
+        PROJECT_PATH = '/home/user/projects'
+        OUTPUT_PATH = os.path.join(PROJECT_PATH, 'output')
 
-    # DATABASE_PATH = '/mnt/d/Onedrive/Documents/data'
-    DATABASE_PATH = r'D:\Onedrive\Documents\data'
+        # DATABASE_PATH = '/mnt/d/Onedrive/Documents/data'
+        DATABASE_PATH = '/home/user/data'
+    else:
+        PROJECT_PATH = r'D:\Onedrive\Temp\Projects\CapitalControl'
+        OUTPUT_PATH = os.path.join(PROJECT_PATH, 'regression_data')
+
+        # DATABASE_PATH = '/mnt/d/Onedrive/Documents/data'
+        DATABASE_PATH = r'D:\Onedrive\Documents\data'
+
+        # NCA_DATA_PATH = r'D:\Onedrive\Projects\NCA\data'
+        # NCA_PROJECT_PATH = r'D:\Onedrive\Temp\Projects\NCA'
+        # NCA_TEMP_PATH = os.path.join(NCA_PROJECT_PATH, 'temp')
+        # NCA_OUTPUT_PATH = os.path.join(NCA_PROJECT_PATH, 'regression_data')
+
+    DATA_PATH = os.path.join(PROJECT_PATH, 'data')
+    TEMP_PATH = os.path.join(PROJECT_PATH, 'temp')
+    REGRESSION_RESULT_PATH = os.path.join(PROJECT_PATH, 'regression_results')
     CSMAR_PATH = os.path.join(DATABASE_PATH, 'csmar')
     SHANGDAO_PATH = os.path.join(DATABASE_PATH, 'shangdao')
     COMPUSTAT_PATH = os.path.join(DATABASE_PATH, 'compustat')
-
-    NCA_DATA_PATH = r'D:\Onedrive\Projects\NCA\data'
-    NCA_PROJECT_PATH = r'D:\Onedrive\Temp\Projects\NCA'
-    NCA_TEMP_PATH = os.path.join(NCA_PROJECT_PATH, 'temp')
-    NCA_OUTPUT_PATH = os.path.join(NCA_PROJECT_PATH, 'regression_data')
